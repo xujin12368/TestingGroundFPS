daabf0fd029f081fbf864a967df5973fed06d0f1
xunigel <xujin12368@163.com>
2019-04-12 23:12:28 +0800

    重构项目文件夹时，引起的GameMode路径读取错误，已修改

--- Source/TestingGroundFPS/TestingGroundFPSGameMode.cpp
+++ Source/TestingGroundFPS/TestingGroundFPSGameMode.cpp
@@ -9,7 +9,7 @@ ATestingGroundFPSGameMode::ATestingGroundFPSGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Blueprints/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
