5d11cf5f5653cfb3f6a1931014d41463837acdfa
xunigel <xujin12368@163.com>
2019-04-13 22:29:39 +0800

    项目目录重构完成。

--- Source/TestingGroundFPS/TestingGroundFPSGameMode.cpp
+++ Source/TestingGroundFPS/TestingGroundFPSGameMode.cpp
@@ -9,7 +9,7 @@ ATestingGroundFPSGameMode::ATestingGroundFPSGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Character/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
