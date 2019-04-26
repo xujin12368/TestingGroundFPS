03b9573abffa2fd2e8c07658ad7b9f7c638ed671
xunigel <xujin12368@163.com>
2019-04-26 11:30:48 +0800

    Updated LFS.Completely.

--- Source/TestingGroundFPS/TestingGroundFPSHUD.cpp
+++ Source/TestingGroundFPS/TestingGroundFPSHUD.cpp
@@ -10,7 +10,7 @@
 ATestingGroundFPSHUD::ATestingGroundFPSHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Static/Player/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
